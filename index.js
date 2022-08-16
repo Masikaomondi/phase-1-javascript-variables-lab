fd1ffbd069d57822981c2c0b3218cbf952b8a7b2
Masikaomondi <masikaomondi@gmail.com>
2022-08-16 09:33:59 +0300

    Completed assisgment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file!
- let companyName = 'Scuber';
-cost companyName = ('Scuber');
+ cost companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
 
